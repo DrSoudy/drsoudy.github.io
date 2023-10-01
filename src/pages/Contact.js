728f7a111680afa9ce8bc355250abd7d23160a21
Hassan El-seoudy <hassan.elseoudy@gmail.com>
2023-10-01 20:22:58 +0200

    :memo: Mohamed's identity

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ import ContactIcons from '../components/Contact/ContactIcons';
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ hi@mldangelo.com"
+    description="Contact Mohamed Elsoudy via email @ Muhammed.elsoudy@gmail.com"
   >
     <article className="post" id="contact">
       <header>
