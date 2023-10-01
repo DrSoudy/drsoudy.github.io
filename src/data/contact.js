09276651045cfe52c0b37e9b7136e3cd24527842
Hassan El-seoudy <hassan.elseoudy@gmail.com>
2023-10-01 20:08:41 +0200

    :memo: Update contacts

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,25 +1,25 @@
-import {faFacebookF} from '@fortawesome/free-brands-svg-icons/faFacebookF';
-import {faLinkedinIn} from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
-import {faEnvelope} from '@fortawesome/free-regular-svg-icons/faEnvelope';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 
 const data = [
-    {
-        link: 'https://www.facebook.com/mohamed.aelsoudy.16',
-        label: 'Facebook',
-        icon: faFacebookF,
-    },
-    {
-        link: 'https://www.linkedin.com/in/mohamedelsoudy',
-        label: 'LinkedIn',
-        icon: faLinkedinIn,
-    },
-    {
-        link: 'mailto:Muhammed.elsoudy@gmail.com',
-        label: 'Email',
-        icon: faEnvelope,
-    },
+  {
+    link: 'https://www.facebook.com/mohamed.aelsoudy.16',
+    label: 'Facebook',
+    icon: faFacebookF,
+  },
+  {
+    link: 'https://www.linkedin.com/in/mohamedelsoudy',
+    label: 'LinkedIn',
+    icon: faLinkedinIn,
+  },
+  {
+    link: 'mailto:Muhammed.elsoudy@gmail.com',
+    label: 'Email',
+    icon: faEnvelope,
+  },
 ];
 
 export default data;
