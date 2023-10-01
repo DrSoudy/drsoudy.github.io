e4870777924f644c94498d5e5414151608484303
Hassan El-seoudy <hassan.elseoudy@gmail.com>
2023-10-01 21:42:01 +0200

    :fire: Fire Email link

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
-import EmailLink from '../components/Contact/EmailLink';
 import ContactIcons from '../components/Contact/ContactIcons';
 
 const Contact = () => (
@@ -17,8 +16,9 @@ const Contact = () => (
         </div>
       </header>
       <div className="email-at">
-        <p>Feel free to get in touch. You can email me at: </p>
-        <EmailLink />
+        <p>Have any questions or inquiries? I would love to hear from you! Feel free to get in touch
+          with me using the contact information provided below.
+        </p>
       </div>
       <ContactIcons />
     </article>
