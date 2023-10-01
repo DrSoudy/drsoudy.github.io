6ba44bd6a997fcc3a52238f59a1438f6d4b3836d
Hassan El-seoudy <hassan.elseoudy@gmail.com>
2023-10-01 20:27:04 +0200

    :memo: Update Certificates

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -1,144 +1,67 @@
 const courses = [
-  {
-    title: 'Convex Optimization',
-    number: 'EE 364a',
-    link: 'http://stanford.edu/class/ee364a/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Machine Learning',
-    number: 'CS 229',
-    link: 'http://cs229.stanford.edu/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Convolutional Neural Networks for Visual Recognition',
-    number: 'CS 231n',
-    link: 'http://cs231n.stanford.edu/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Numerical Linear Algebra',
-    number: 'CME 302',
-    link:
-      'http://scpd.stanford.edu/search/publicCourseSearchDetails.do;jsessionid=561188A06434D7D97953C4706DE12831?method=load&courseId=11685',
-    university: 'Stanford',
-  },
-  {
-    title: 'Numerical Optimization',
-    number: 'CME 304',
-    link: 'http://web.stanford.edu/class/cme304/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Discrete Mathematics and Algorithms',
-    number: 'CME 305',
-    link: 'http://stanford.edu/~rezab/discrete/',
-    university: 'Stanford',
-  },
-  {
-    title: 'PDE’s of Applied Mathematics',
-    number: 'CME 303',
-    link: 'http://web.stanford.edu/class/math220/index.html',
-    university: 'Stanford',
-  },
-  {
-    title: 'Introduction to Linear Dynamical Systems',
-    number: 'EE 263',
-    link: 'http://ee263.stanford.edu/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Introduction to Statistical Signal Processing',
-    number: 'EE 278B',
-    link: 'http://web.stanford.edu/class/ee278/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Spacecraft Design',
-    number: 'AA 236A',
-    link: '',
-    university: 'Stanford',
-  },
-  {
-    title: 'Advanced Programming',
-    number: 'CME 212',
-    link: '',
-    university: 'Stanford',
-  },
-  {
-    title: 'Design Theory and Methodology',
-    number: 'MAE 397',
-    link:
-      'https://web.stanford.edu/group/designx_lab/cgi-bin/mainwiki/index.php/ME397_Design_Theory_%26_Methodology_Seminar',
-    university: 'Stanford',
-  },
-  {
-    title: 'Software Engineering Concepts',
-    number: 'CSE 442',
-    link: '',
-    university: 'Buffalo',
-  },
-  {
-    title: 'Hardware/Software Integrated System Design',
-    number: 'CSE 453',
-    link: '',
-    university: 'Buffalo',
-  },
-  {
-    title: 'Data Structures',
-    number: 'CS 250',
-    link: 'http://www.cse.buffalo.edu/~hungngo/classes/2013/Fall/250/',
-    university: 'Buffalo',
-  },
-  {
-    title: 'Introduction to Digital Signal Processing',
-    number: 'EE 516',
-    link: '',
-    university: 'Buffalo',
-  },
-  {
-    title: 'Computer Vision and Image Processing',
-    number: 'CSE 573',
-    link: 'http://cubs.buffalo.edu/~inwogu/teaching/Coursepage573_fa14/',
-    university: 'Buffalo',
-  },
-  {
-    title: 'Realtime Embedded Systems',
-    number: 'CSE 321',
-    link: '',
-    university: 'Buffalo',
-  },
-  {
-    title: 'Computer Architecture',
-    number: 'CSE 590',
-    link: 'http://www.cse.buffalo.edu/~stevko/courses/cse490/spring11/',
-    university: 'Buffalo',
-  },
-  {
-    title: 'Small Data',
-    number: 'MS&E 226',
-    link: 'http://web.stanford.edu/class/msande226/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Stochastic Control',
-    number: 'EE 266',
-    link: 'http://ee266.stanford.edu/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Simulation',
-    number: 'MS&E 223',
-    link: 'http://web.stanford.edu/class/msande223/handout.htm',
-    university: 'Stanford',
-  },
-  {
-    title: 'Deep Learning for Natural Language Processing',
-    number: 'CS 224d',
-    link: 'http://cs224d.stanford.edu/',
-    university: 'Stanford',
-  },
-];
-
-export default courses;
+    {
+        title: 'Basic Selling Skills',
+        link: ' https://drive.google.com/file/d/1f7ouWrz42U7fl-5CrjvKbaNM8twNvUYr/view?usp=sharing',
+        university: 'Tag Pharma',
+    },
+    {
+        title: 'Human Development',
+        link: 'https://drive.google.com/file/d/1MMax8A8Gvx0MdLUtr_jV7Z4_s-gMhjLe/view?usp=sharing',
+        university: 'Abninafsak',
+    },
+    {
+        title: 'Over The Counter drugs used in most common diseases',
+        link: ' https://drive.google.com/file/d/1fYYzWnrZFwROLSPxgygLoLUZetLg3EI_/view?usp=sharing',
+        university: 'AADP',
+    },
+    {
+        title: 'Medical Promotion',
+        link: ' https://drive.google.com/file/d/1ypfOgbnAbEF0WehRZ9_oF38TTnzq0aKF/view?usp=sharing',
+        university: 'Eva Pharma Company',
+    },
+    {
+        title: 'Advanced presentation and selling skills',
+        link: ' https://drive.google.com/file/d/1jZF8NbgSYsRvdRt--QmtXc3BEe8tVWtB/view?usp=sharing',
+        university: 'Doctorz Company for Perfumes and Cosmetics',
+    },
+    {
+        title: 'Practical Training in Market & Achieve some sales',
+        link: 'https://drive.google.com/file/d/1P-m0zGR7UzSHkPUGfy30qylo4SiRTsQI/view?usp=sharing',
+        university: 'Doctorz Company for Perfumes and Cosmetics',
+    },
+    {
+        title: 'CV Writing',
+        link: 'https://drive.google.com/file/d/1KiHUPY4bC_Fo-bPwFH-PwWw81dgYY1be/view?usp=sharing',
+        university: 'Pharos University',
+    },
+    {
+        title: 'Time Management',
+        link: 'https://drive.google.com/file/d/1xz2zj6B88q3VRbqlCTbPGR_RpQ37TMGt/view?usp=sharing',
+        university: 'Pharos',
+    },
+    {
+        title: 'Entrepreneurship Workshop',
+        link: 'https://drive.google.com/file/d/1SQZyC6mR_uETgzREQqu47nkJq6kyNnkY/view?usp=sharing',
+        university: 'Pharos',
+    },
+    {
+        title: 'Clinical Pharmacy Training',
+        link: 'https://drive.google.com/file/d/1ItoMiZJGu262kTztTnEmHZjFnbmPkInx/view?usp=sharing',
+        university: 'Alex. Armed Force Hospital',
+    },
+    {
+        title: 'Sales internship',
+        link: 'https://drive.google.com/file/d/1_K6gpUA67rYTosvv0lvPadb5BAqJCQiw/view?usp=sharing',
+        university: 'Nestle',
+    },
+    {
+        title: 'Medical Sales Internship',
+        link: 'https://drive.google.com/file/d/1uP101IerLCf7jqrMqUJled1Mzm5GQbYm/view?usp=sharing',
+        university: 'Sanofi',
+    },
+    {
+        title: 'Hepatitis C Testing',
+        link: 'https://drive.google.com/file/d/1NH5pRgUvHwgdokUIEn5awN-er1XZ7UXY/view?usp=sharing',
+        university: 'Ministry of Health and Population',
+    }
+]
