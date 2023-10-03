21cfdf5e6c9e7b3f3409bb76a4c9b4c0494fb189
Hassan El-seoudy <hassan.elseoudy@gmail.com>
2023-10-04 01:11:10 +0200

    :pencil2: Update work.

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -13,158 +13,123 @@
  */
 const work = [
   {
-    name: 'Smile Identity',
-    position: 'VP Engineering & Head of AI',
-    url: 'https://smileidentity.com',
-    startDate: '2022-01-01',
-    summary: `Smile builds machine learning APIs to compare user submitted photos to ID documents. Our APIs are
-    used by hundreds of thousands of people every day to access financial services. As VP of Engineering,
-    I provide technical leadership on high-impact projects, influence and coach a distributed team of engineers,
-    and facilitate alignment and clarity across teams on goals, outcomes, and timelines. I was promoted
-    from Director of Engineering to VP of Engineering in April 2022, and then to VP of Engineering and Head
-    of AI in November 2022. I lead a 20+ person engineering org. I directly manage ~8 engineers and spend
-    >50% of my time writing code.`,
+    name: 'Alexandria University Hospitals',
+    position: 'Hospital Pharmacist',
+    url: 'https://auhospitals.alexu.edu.eg',
+    startDate: '2023-03-01',
+    summary: `Experienced pharmacist skilled in dispensing medications, monitoring therapy, 
+    and providing drug information. Proficient in clinical trials and research for innovative 
+    treatments. Expertise in patient care areas such as nutrition support and pain management.`,
     highlights: [
-      'Redesigned engineering processes for bug tracking, meetings, and standups. Improved culture for code reviews, blameless post-mortems, and retrospectives.',
-      'Re-architected engineering hiring and onboarding processes. Recruited several strong engineers.',
-      'Reorged engineering teams to focus on product delivery. Created a new team to focus on ML infrastructure.',
-      'Lead re-design of internal APIs for inference. Built new computer vision pipelines for industry leading certifications (NIST/iBeta liveness).',
-      'Pitched, designed, developed, deployed, and maintain a fraud detection product based on 1-N facial recognition using embeddings and vector search.',
+      'Dispense medications to patients, both in-house and upon discharge.',
+      'Monitor patients\' medication therapy to identify and resolve any potential problems.',
+      'Provide drug information to other healthcare providers and patients.',
+      'Involved in other areas, such as nutrition support, and antimicrobial stewardship.'
     ],
   },
   {
-    name: 'Skeptical Investments',
-    position: 'Co-founder',
-    url: 'http://skepticalinvestments.biz',
-    startDate: '2017-04-01',
-    summary: `Skeptical Investments is a micro-vc fund focused on Angel, Seed, and Series-A
-    YC companies.`,
+    name: 'Institute of Training and Occupational Learning (ITOL)',
+    position: 'Project Executive (Part-Time)',
+    url: 'https://www.itol.org',
+    startDate: '2023-01-01',
+    summary: `Institute of Training and Occupational Learning. Home of training, learning, and 
+    continuing professional development.`,
     highlights: [
-      'Created the InstaSafe. Fast, automated investment docs for YC companies.',
-      'Advise several startups in the fund.',
+      'Establish excellent relationships with customers in B2B.',
+      'Perform accreditation processes for training programs.',
     ],
   },
   {
-    name: 'Arthena',
-    position: 'Co-founder & C.T.O.',
-    url: 'https://arthena.com',
-    startDate: '2014-01-01',
-    endDate: '2022-01-01',
-    summary: `Arthena is a Series A Company funded by <a href="https://www.anthemis.com/">Athemis</a>,
-    <a href="https://foundationcapital.com">Foundation Capital</a>, and <a href="https://ycombinator.com/">YC</a>.
-    We develop quantitative strategies to predict the value of fine art and build investment products.
-    I ran a 20-person product and engineering org for 8 years. We were acquired by
-    <a href="https://www.masterworks.com/">Masterworks</a> in 2023.`,
+    name: 'Soudy Store for perfumes and cosmetics (Own business)',
+    position: 'Business Owner, seller, and formulator',
+    url: 'https://www.facebook.com/SoudyStore',
+    startDate: '2019-09-01',
+    endDate: '2023-01-01',
+    summary: `Soudy Store is a small store for agency and distribution of perfumes and cosmetics. 
+    As business owner, I had to develop a business plan, manage staff, market the business online 
+    and offline, provide customer service, and comply with all applicable regulations. In addition, 
+    I used my pharmaceutical experience to formulate some fragrances and cosmeceutical formulations. 
+    As salesperson, I had to promote and make deals with B2B and B2C customers.`,
     highlights: [
-      'Built production, online, end-to-end optimized machine learning pipelines for time-series prediction.',
-      'Designed micro-service architecture around: data collection, data integrity, feature engineering, research, strategy, backtesting, deployment, and reporting.',
-      "Set and communicated team priorities that supported the broader organization's goals. Aligned strategy, processes, and decision-making across teams.",
-      "Set clear expectations with individuals based on their level and role and aligned them to the broader organization's goals. Met regularly with individuals to discuss performance and development, and provided feedback and coaching.",
-      'Developed the long-term technical vision and roadmap within, and often beyond, the scope of my teams. Evolved the roadmap to meet anticipated future requirements and infrastructure needs.',
+      'Built my brand called سعودي.',
+      'Covered all costs of my store and succeeded in gaining revenue and profit.',
+      "Established a database of more than 1000 customers in 7 governorates.",
+      "Formulated some fragrance products like eau de cologne, eau de toilet, and eau de perfume.",
+      'Made more than 300,000 L.E. sales in one product throughout the last years.',
     ],
   },
   {
-    name: 'Enveritas',
-    position: 'ML Engineering Contractor',
-    url: 'http://enveritas.org',
-    startDate: '2016-09-01',
-    endDate: '2018-01-01',
-    summary: `Enveritas is a non-profit that works with coffee farmers to improve their livelihoods.
-    I joined on day one and helped build a variety of tools to collect and analyze data. I also advised
-    on technical strategy, helped hire engineers, and advised on their YC application.`,
+    name: 'National Food Safety Authority (NFSA)',
+    position: 'Special Food Safety Inspector ',
+    url: 'https://reg.nfsa.gov.eg',
+    startDate: '2021-08-01',
+    endDate: '2023-02-01',
+    summary: `A special food safety inspector with pharmaceutical background is a public health 
+    professional who is responsible for ensuring that special food is safe to deliver and eat. 
+    In addition, inspection and sampling of special foods (Food supplements, infant formulas, etc.) 
+    and review and draft regulatory documents required for importing these products.`,
     highlights: [
-      'Trained a model for the Brazilian Coffee Scenes Dataset with better than state of the art accuracy.',
-      'Collected training sets on the ground in Uganda. Built dashboards to visualize work of surveyors using Flask, React, and D3.',
-      'Performed supporting analysis to ensure data integrity using Pandas, t-SNE, SVM\'s, and other techniques.',
+      'Established a sub-unit cooperatively resulting in reducing time of inspection by half.',
     ],
   },
   {
-    name: 'Zenysis',
-    position: 'Software Engineering Contractor',
-    url: 'https://zenysis.com',
-    startDate: '2016-02-01',
-    endDate: '2016-03-01',
-    summary: `Zenysis develops data analysis and visualization tools for government agencies and international organizations.
-    Their solutions help improve decision-making by integrating and analyzing large-scale datasets from various sources.
-    I was the second contributor to the codebase. We built data visualization tools and tackled complex data ingestion
-    challenges for the Ethiopian Ministry of Health.`,
+    name: 'Asmaa Abo Hay Pharmacy',
+    position: 'Community Pharmacist',
+    url: 'https://www.facebook.com/asmaaabohaypharmacy',
+    startDate: '2021-05-01',
+    endDate: '2021-07-01',
+    summary: `Asmaa Abo Hay is a pharmacy in Damanhour city. 
+    Being a community pharmacy made me play an important role in providing convenient and 
+    accessible healthcare services to the public. As known, community pharmacists are often the 
+    first point of contact for people with health concerns, and they can provide advice and support 
+    on a wide range of topics.`,
     highlights: [
-      'Worked in Addis Ababa for the Ethiopian Ministry of Health and built data visualization tools in React and Flask.',
-      'Ingested multiple databases with different alphabets, calendars, and without official spellings of geographic locations.',
+      'Managed the pharmacy during my night shift.',
     ],
   },
   {
-    name: 'Matroid',
-    position: 'Co-founder',
-    url: 'https://matroid.com',
-    startDate: '2015-07-01',
-    endDate: '2016-01-01',
-    summary: `Matroid is a computer vision platform for creating and deploying detectors. Detectors help customers identify objects,
-    events, or patterns in images and videos without requiring machine learning or coding expertise. I played a crucial role in
-    defining the company's vision. I was responsible for architecting and building the initial product. I left shortly after the
-    Series A to focus on Arthena.`,
+    name: 'NIG Nutritional Egypt',
+    position: 'Product Specialist',
+    url: 'https://www.nignutritionals.co.nz',
+    startDate: '2021-02-01',
+    endDate: '2021-04-01',
+    summary: `NIG is a multinational company specialized in prophylactic nutrition. 
+    Headquarters in Newzeland. I had to provide technical and clinical information about bioactive 
+    colostrum to healthcare professionals, manage sales relationships with healthcare professionals 
+    and attend medical conferences and events.`,
     highlights: [
-      'Developed end to end machine learning pipeline to train visual classifiers from keywords using Caffe, Node.JS, Redis, MongoDB, and other technologies.',
+      'Achieved sales from the first month.',
     ],
   },
   {
-    name: 'Planet',
-    position: 'Missions Intern',
-    url: 'https://planet.com',
-    startDate: '2014-06-01',
-    endDate: '2015-01-01',
+    name: 'Evy Pharmacy',
+    position: 'Community Pharmacist',
+    url: 'https://evypharmacy.business.site',
+    startDate: '2020-08-01',
+    endDate: '2021-01-01',
+    summary: `Working in a community pharmacy allowed me to assume a significant responsibility 
+    in offering convenient and easily accessible healthcare services to the community.`,
     highlights: [
-      'Built models to improve image quality, signal to noise ratio, and dynamic range.',
-      'Performed statistical analysis of image quality in Matlab and Python.  Developed flight software in C++.',
-      'Organized first hackathon, prototyped hardware, and designed and built photography equipment for rocket launches.',
+      'Compound and dispense medication as prescribed by physician.',
     ],
   },
   {
-    name: 'Planetary Resources',
-    position: 'Avionics Intern',
-    startDate: '2014-01-01',
-    endDate: '2014-05-01',
-    url: 'http://planetaryresources.com',
+    name: 'Doctorz Company for Perfumes and Cosmetics',
+    position: 'Promotional and Sales Representative',
+    startDate: '2019-07-01',
+    endDate: '2020-08-01',
+    url: 'https://www.facebook.com/doctorzcoperfumes',
+    summary: `Doctorz Company for Perfumes and Cosmetics Alexandria and Delta, Egypt
+    A company specialized in perfumes, cosmetics, medical plants and chemicals. I established this 
+    new brand outside Alexandria governorate and fulfilled many tasks in fields of sales, marketing, 
+    customer services and after sales service.`,
     highlights: [
-      'Developed simulations in Matlab for Attitude Determination and Control Subsystem.',
-      'Developed processes for in lab testing and characterization of various subsystems.',
-      'Assembled flight hardware in cleanroom.',
+      'Best achiever in Q4 2019.',
+      'Built Doctorz business outside Alexandria from nothing in 2016 to +350,000 L.E. ' +
+      'in total sales in the last 9 months.',
+      'Q1,2 2020 total sales were 57.3% more than the same period in 2019.',
     ],
-  },
-  {
-    name: 'Facebook',
-    position: 'Software Engineer Intern',
-    url: 'https://facebook.com',
-    startDate: '2013-06-01',
-    endDate: '2013-09-01',
-    highlights: [
-      'Developed software in python for automated testing of servers.',
-      'Performed statistical analysis with R, HIVE to assist in triage of malfunctioning servers.',
-      'Worked with vendors and ODM\'s during triage to assist in risk mitigation.',
-    ],
-  },
-  {
-    name: 'SEDS-USA',
-    position: 'At Large Board Member',
-    url: 'http://seds.org',
-    startDate: '2013-10-01',
-    endDate: '2014-10-01',
-    highlights: [
-      'Elected to Board of Directors of the USA\'s largest student space advocacy group based on 5+ years of work with SEDS.',
-      'Responsibilities included: organizational strategy, conference presentations, fundraising, special projects, promoting SEDS nationally and internationally, and photographing conferences.',
-    ],
-  },
-  {
-    name: 'UB Nanosatellite Program',
-    position: 'Co-founder, Program Manager',
-    url: 'https://ubnl.space/',
-    startDate: '2010-10-01',
-    endDate: '2012-06-01',
-    highlights: [
-      'Coauthored grant to build a multi-spectral imaging satellite as part of the AFRL University Nanosatellite Program.',
-      'Lead a team of 60 students through satellite development life cycle. Served as a technical expert, acquired intimate working knowledge of satellite subsystems.  Solicited funding through NASA, AFOSR, and several corporate sponsors.',
-    ],
-  },
+  }
 ];
 
 export default work;
